890c86d65f1324bd94366a1c67b456d9488f01a8
luisEduardoBet <luiseduardobet22@outlook.com>
2025-03-24 20:18:46 -0300

    almost_UI_done

--- Source/TTEA_menu.py
+++ Source/TTEA_menu.py
@@ -1,362 +1,133 @@
-
-import tkinter as tk
-from tkinter import ttk
+import customtkinter as ctk
+from tkinter.ttk import Separator
+from os import listdir
 from PIL import Image, ImageTk
-import os
-import arquivo
-from settings import *
-import numpy as np
-import settings
-import calibracaov2
-
-#para inserir arquivos de outras pastas 
-from sys import path
-path.insert(1, '/VesTEA')
-
-
-
-
-def center_window_on_screen(width, height):
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    x_cord = int((screen_width/2) - (width/2))
-    y_cord = int((screen_height/2) - (height/2))
-    root.geometry("{}x{}+{}+{}".format(width, height, x_cord, y_cord))
-
-def show_menu():
-    root.title('Menu TTEA')
-    arr_Jogadores = ler_nome_jogadores()
-    # set combo values
-    jogador_cb['values'] = arr_Jogadores
-
-    width, height = 400, 600
-    center_window_on_screen(width, height)
-    menu_frame.pack()
-    cad_frame.forget()
-    game_cb['state'] = 'readonly'
-    game_cb.set('')
-    jogador_cb['state'] = 'disabled'
-    jogador_cb.set('')
-    fase_cb['state'] = 'disabled'
-    fase_cb.set('')
-    nivel_cb['state'] = 'disabled'
-    nivel_cb.set('')
-
-
-def show_cad():
-    root.title('Cadastro TTEA')
-    width, height = 300, 150
-    center_window_on_screen(width, height)
-    cad_frame.pack()
-    menu_frame.forget()
-
-root = tk.Tk()
-
-# config the root window
-root.resizable(False, False)
-root.title('Menu TTEA')
-width, height = 400, 600
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-center_window_on_screen(width, height)
-
-# frames
-menu_frame = tk.Frame(root)
-cad_frame = tk.Frame(root)
-
-# Menu
-
-# Logo
-image = Image.open("Assets/TTEA Logo.png")
-photo = ImageTk.PhotoImage(image)
-imagem = tk.Label(menu_frame, text = "TTEA Logo", image = photo)
-imagem.image = photo
-imagem.pack()
-
-
-# Calibrar Button
-def CalibrarCallback():
-    #import calibracao
-    calibracaov2.calibrar_ttea()
-
-
-B = tk.Button(menu_frame, text ="Calibrar", command = CalibrarCallback)
-
-B.pack()
-
-# label
-label = ttk.Label(menu_frame, text="Jogos:")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-# create a combobox
-selected_game = tk.StringVar()
-game_cb = ttk.Combobox(menu_frame, textvariable=selected_game)
-game = ''
-
-# set combo values
-game_cb['values'] = ['KARTEA', 'REPETEA', 'VESTEA'
-]
-
-# prevent typing a value
-game_cb['state'] = 'readonly'
-
-# place the widget
-game_cb.pack(fill=tk.X, padx=100, pady=5)
-
+from CTkMenuBar import menu_bar
 
-# bind the selected value changes
-def game_changed(event):
-    global game
-    game = selected_game.get()
-    jogador_cb['state'] = 'readonly'
-    jogador_cb.set('')
-    if game == 'KARTEA':
-        fase_cb['values'] = ['1', '2', '3']
-        nivel_cb['values'] = ['1', '2', '3', '4', '5', '6']
-    elif game == 'REPETEA':
-        fase_cb['values'] = ['1', '2', '3','4','5','6','7','8','9','10']
-        nivel_cb['values'] = ['1', '2', '3', '4', '5']
-    elif game == 'VESTEA':
-        fase_cb['values'] = ['1', '2', '3']
-        nivel_cb['values'] = ['1', '2', '3', '4', '5','6','7','8','9','10', '11', '12', '13','14','15']
-
-    fase_cb['state'] = 'disabled'
-    fase_cb.set('')
-    nivel_cb['state'] = 'disabled'
-    nivel_cb.set('')
-
-game_cb.bind('<<ComboboxSelected>>', game_changed)
-
-
-# label
-label = ttk.Label(menu_frame, text="Jogador:")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-# create a combobox
-selected_jogador = tk.StringVar()
-jogador_cb = ttk.Combobox(menu_frame, textvariable=selected_jogador)
-
-arr_Jogadores = []
-
-def ler_nome_jogadores():
-    # Reading registered players
-    path = os.getcwd() + "\Jogadores"
-    Jogadores = os.listdir(path)
-    #print(Jogadores)
-
-    arr = []
-    b = ''
-
-    for a in Jogadores:
-        a = a.replace('_KarTEA_sessao.csv','')
-        a = a.replace('_KarTEA_config.csv','')
-        a = a.replace('_KarTEA_detalhado.csv','')
-        a = a.replace('_RepeTEA.csv','')
-        a = a.replace('_RepeTEA_config.csv','')
-        a = a.replace('_RepeTEA_detalhado.csv','')
-        a = a.replace('_VesTEA_sessao.csv','')
-        a = a.replace('_VesTEA_config.csv','')
-        a = a.replace('_VesTEA_detalhado.csv','')
-        if a != b:
-            arr.append(a)
-        b = a
-    return arr
 
-#print(arr_Jogadores)
-arr_Jogadores = ler_nome_jogadores()
-# set combo values
-jogador_cb['values'] = arr_Jogadores
+class SidebarFrame(ctk.CTkFrame): 
+    def __init__(self, master, games): 
+        super().__init__(master)
+        
 
-# prevent typing a value
-jogador_cb['state'] = 'disabled'
+        self.rowconfigure((0,1,2,3,4,5),weight=1) 
 
-# place the widget
-jogador_cb.pack(fill=tk.X, padx=100, pady=5)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self._corner_radius = 0
 
 
-# bind the selected value changes
-jogador = ''
-FASE = 0
-NIVEL = 0
-PLAYER_ARQ_CONFIG = ''
-
-def jogador_changed(event):
-    global jogador
-    jogador = selected_jogador.get()
-    global PLAYER_ARQ_CONFIG
-    PLAYER = "Jogadores/" + jogador
-    if game == 'KARTEA':
-        PLAYER_ARQ = PLAYER + "_KarTEA_sessao.csv"
-        PLAYER_ARQ_CONFIG = PLAYER + "_KarTEA_config.csv"
-        PLAYER_ARQ_DET = PLAYER + "_KarTEA_detalhado.csv"
-    elif game == 'REPETEA':
-        PLAYER_ARQ = PLAYER + "_RepeTEA_sessao.csv"
-        PLAYER_ARQ_CONFIG = PLAYER + "_RepeTEA_config.csv"
-        PLAYER_ARQ_DET = PLAYER + "_RepeTEA_detalhado.csv"
-    elif game == 'VESTEA':
-        PLAYER_ARQ = PLAYER + "_VesTEA_sessao.csv"
-        PLAYER_ARQ_CONFIG = PLAYER + "_VesTEA_config.csv"
-        PLAYER_ARQ_DET = PLAYER + "_VesTEA_detalhado.csv"
-
-    global FASE, NIVEL
-    FASE = arquivo.get_K_FASE(PLAYER_ARQ_CONFIG)
-    NIVEL = arquivo.get_K_NIVEL(PLAYER_ARQ_CONFIG)
-    fase_cb['state'] = 'readonly'
-    fase_cb.current(FASE-1)
-    nivel_cb['state'] = 'readonly'
-    nivel_cb.current(NIVEL-1)
-
-
-jogador_cb.bind('<<ComboboxSelected>>', jogador_changed)
-
-
-
-def cadastrarCallback():
-    show_cad()
-
-
-B = tk.Button(menu_frame, text ="Cadastrar Novo Jogador", command = cadastrarCallback)
-
-B.pack(fill=tk.X, padx=100, pady=10)
-
-# label
-label = ttk.Label(menu_frame, text="Fase:")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-# create a combobox
-selected_fase = tk.StringVar()
-fase_cb = ttk.Combobox(menu_frame, textvariable=selected_fase)
-
-# set combo values
-#fase_cb['values'] = ['1', '2', '3']
-
-# prevent typing a value
-fase_cb['state'] = 'disabled'
-
-# place the widget
-fase_cb.pack(fill=tk.X, padx=100, pady=5)
-
-# bind the selected value changes
-def fase_changed(event):
-    arquivo.set_K_FASE(PLAYER_ARQ_CONFIG, int(selected_fase.get()))
-
-fase_cb.bind('<<ComboboxSelected>>', fase_changed)
-
-
-# label
-label = ttk.Label(menu_frame, text="Nível:")
-label.pack(fill=tk.X, padx=100, pady=5)
-
-# create a combobox
-selected_nivel = tk.StringVar()
-nivel_cb = ttk.Combobox(menu_frame, textvariable=selected_nivel)
-
-# set combo values
-nivel_cb['values'] = ['1', '2', '3', '4', '5', '6']
-
-# prevent typing a value
-nivel_cb['state'] = 'disabled'
-
-# place the widget
-nivel_cb.pack(fill=tk.X, padx=100, pady=5)
-
-nivel = ''
-# bind the selected value changes
-def nivel_changed(event):
-    arquivo.set_K_NIVEL(PLAYER_ARQ_CONFIG, int(selected_nivel.get()))
-
-nivel_cb.bind('<<ComboboxSelected>>', nivel_changed)
-
-
-def JogarCallback():
-    arquivo.set_Player(jogador)
-    arquivo.set_Fase(arquivo.get_K_FASE(PLAYER_ARQ_CONFIG))
-    arquivo.set_Nivel(arquivo.get_K_NIVEL(PLAYER_ARQ_CONFIG))
-    print("Jogador: ", arquivo.get_Player(), " Fase: ", arquivo.get_Fase(), " Nivel: ", arquivo.get_Nivel())
-    settings.pontos_calibracao = arquivo.lerCalibracao()
-    x1 = settings.pontos_calibracao[2][0]
-    x2 = settings.pontos_calibracao[3][0]
-    print("x1= ", x1, "x2= ", x2)
-    settings.div0_pista = 0
-    settings.div1_pista = (SCREEN_WIDTH // 3)
-    settings.div2_pista = (2*(SCREEN_WIDTH//3))
-    settings.div3_pista = SCREEN_WIDTH
-    print("Pontos de Calibracao: ", settings.pontos_calibracao)
-    print("Div0: ", settings.div0_pista, " Div1: ", settings.div1_pista,"Div2: ", settings.div2_pista, " Div3: ", settings.div3_pista)
-
-    TARGETS_MOVE_SPEED = arquivo.get_Nivel()
-
-    if game == 'KARTEA':
-        import KarTEA
-        KarTEA.main()
-    elif game == 'REPETEA':
-        import RepeTEA
-        RepeTEA.main()
-    elif game == 'VESTEA':
-        from VesTEA import vestea_inicio
-        vestea_inicio.main(jogador)
-
-
-B = tk.Button(menu_frame, text ="Jogar", command = JogarCallback)
-
-B.pack()
-
-# Teste-Logo UDESC
-imageUdesc = Image.open("Assets/Logos UDESC Larva.png")
-photoUdesc = ImageTk.PhotoImage(imageUdesc)
-imagemUdesc = tk.Label(menu_frame, text = "Logo Udesc", image = photoUdesc)
-imagemUdesc.imageUdesc = photoUdesc
-imagemUdesc.pack(pady=10)
-
-menu_frame.pack()
-
-#Frame Cadastro
-arr_Jogadores = ler_nome_jogadores()
-
-NomeString = tk.StringVar(cad_frame)
-DataString = tk.StringVar(cad_frame)
-ObsString = tk.StringVar(cad_frame)
-
-LNome = tk.Label(cad_frame, text="Nome: ")
-LNome.grid(column=0, row=0, sticky=tk.W)
-Nome = tk.Entry(cad_frame, width=20, textvariable=NomeString)
-Nome.grid(column=1, row=0, padx=10)
-
-LData = tk.Label(cad_frame, text="Data de Nasc.: ")
-LData.grid(column=0, row=1, sticky=tk.W)
-Data = tk.Entry(cad_frame, width=20, textvariable=DataString)
-Data.grid(column=1, row=1, padx=10)
-
-LObs = tk.Label(cad_frame, text="Observação: ")
-LObs.grid(column=0, row=2, sticky=tk.W)
-Obs = tk.Entry(cad_frame, width=20, textvariable=ObsString)
-Obs.grid(column=1, row=2, padx=10)
-
-def cadastrarcallback():
-    SNome = NomeString.get()
-    SData = DataString.get()
-    SObs = ObsString.get()
-    #print(SNome, SData, SObs)
-
-
-    if SNome not in arr_Jogadores:
-        arquivo.CadastrarJogador(SNome, SData, SObs)
-        arr_Jogadores.append(SNome)
-        res = tk.messagebox.askquestion (title='Jogador cadastrado!', message='Jogador cadastrado com sucesso!\nDeseja cadastrar outro jogador?')
-        if res == 'no':
-            show_menu()
-    else:
-        tk.messagebox.showerror(title='Erro!', message='Jogador com esse nome já esta cadastrado!')
-
-B = tk.Button(cad_frame, text="Cadastrar Novo Jogador", command=cadastrarcallback)
-
-B.grid(column=0, row=3, padx=10, pady=10, sticky=tk.W)
-
-def cancelarcallback():
-    show_menu()
-
-B = tk.Button(cad_frame, text="Cancelar", command=cancelarcallback)
-
-B.grid(column=1, row=3, padx=10, pady=10, sticky=tk.W)
-
-root.mainloop()
+        image = Image.open(r"menu_assets\ttea-logo.png")
+        image = ImageTk.PhotoImage(image)
+
+        self.label_title = ctk.CTkLabel(self,image= image, text = None)
+        self.label_title.grid(row=0, column = 0, padx = 20)
+        self.button = []
+
+        print(self.size())
+
+        home = ctk.CTkButton(self,text="Cadastro", font=(20,20)) 
+        home.grid(row = 1, column = 0, padx = 50, sticky = "ew")
+        self.button.append(home)
+
+
+        for _ in games:
+            temp = ctk.CTkButton(self,text=_, font=(20,20))
+            temp.grid(row= games.index(_)+2, column = 0,  padx = 50, sticky = "ew")
+            self.button.append(temp)
+
+
+
+class MenuSettings(ctk.CTkFrame): 
+    def __init__(self, master): 
+        super().__init__(master)
+
+        self._corner_radius = 0
+
+        self.rowconfigure((0,1,2,3,4), weight = 1)
+        self.columnconfigure((0,1), weight = 1)
+
+        self.label_title = ctk.CTkLabel(self, text = "Configurações da Interface", font=(20,20))
+        self.label_title.grid(row = 0, column = 0, padx = 5, columnspan =2, sticky = "nsew")
+
+
+        self.label_scale = ctk.CTkLabel(self, text = "Escala da Interface", font=(15,15))
+        self.label_scale.grid(row = 1, column = 0, columnspan = 2, padx = 5, sticky = "s")
+
+        self.menu_scale = ctk.CTkOptionMenu(self, values=["80%", "90%","100%", "110%", "120%"], command=None)
+        self.menu_scale.grid(row=2, column = 0, columnspan = 2, padx=50)
+
+        self.label_calibration = ctk.CTkLabel(self, text = "Calibração", font = (15,15))
+        self.label_calibration.grid(row = 3, column = 0, columnspan = 2, padx = 5, sticky = "s")
+
+        self.button_auto_cal = ctk.CTkButton(self, text = "Automática")
+        self.button_auto_cal.grid(row=4, column = 0, padx = 5, sticky= "e")
+
+        self.button_manual_cal = ctk.CTkButton(self, text = "Manual")
+        self.button_manual_cal.grid(row=4, column = 1, padx = 5, sticky= "w")
+
+class RegisterMenu(ctk.CTkFrame): 
+     def __init__(self, master): 
+        super().__init__(master)
+
+        self.rowconfigure(0, weight = 1)
+        self.columnconfigure(0, weight=1)
+
+        self.label_version = ctk.CTkLabel(self, text = "Versão 2.0 - Release 22/03/2025", font = (10,10))
+        self.label_version.grid(row = 3, column = 0, columnspan = 2, padx = 5, sticky = "s")
+
+
+class RepeTEAMenu(ctk.CTkFrame):  
+
+    def __init__(self, master): 
+        super().__init__(master)
+        
+        self._corner_radius = 0
+
+        self.columnconfigure(0, weight= 1)
+        self.rowconfigure (0, weight= 1)
+
+        self.nav_bar =  ctk.CTkTabview(self)
+
+        self.nav_bar.add("Jogar")
+        self.nav_bar.add("Configurações")
+        self.nav_bar.add("Manual")
+
+        self.nav_bar.grid(row =0, column = 0, sticky = "nsew")
+
+
+
+class App(ctk.CTk): 
+    def __init__(self): 
+        super().__init__()
+
+
+        self.geometry("800x600")
+        self.title("T-TEA")
+        self.games = listdir(r"Games")
+
+        self.columnconfigure(0, weight= 1)
+        self.columnconfigure(1, weight= 9)
+        self.rowconfigure ((0,1,2), weight= 5)
+        self.rowconfigure(3)
+
+
+        
+        self.sidebar_frame = SidebarFrame(self, self.games)
+        self.sidebar_frame.grid(row = 0, column=0, rowspan = 2,  sticky = "nsew")
+
+        self.sidebar_settings = MenuSettings(self)
+        self.sidebar_settings.grid(row=2, column=0, sticky = "nsew")
+
+        self.game_menu = RepeTEAMenu(self)
+        self.game_menu.grid(row = 0, column = 1, rowspan = 3, sticky = "nsew")
+
+        self.register_form =  RegisterMenu(self)
+        self.register_form.grid (row=3, column=0, columnspan=2, sticky = 'e')
+
+
+app = App()
+app.mainloop()
+
+
+    
